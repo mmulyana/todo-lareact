b3788cb88d68033c93d692c1f56e6f29a108033f
mmulyana <mulyan.t20@gmail.com>
2024-04-01 11:00:00 +0700

    feat: setup model and seeder project and task

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'id' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => time()
+        ]);
+        User::factory()->create([
+            'id' => 2,
+            'name' => 'mulyana',
+            'email' => 'mulyana@mail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => time()
+        ]);
+
+        Project::factory()->count(10)->hasTasks(10)->create();
+
+    }
+}
