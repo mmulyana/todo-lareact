5aaa99cc25559f37401bacab871c46d8f0864aa2
mmulyana <mulyan.t20@gmail.com>
2024-04-09 08:00:00 +0700

    feat: add table for projects

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -3,6 +3,7 @@ import { Head } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
     const { data } = projects;
+    console.log(data)
     return (
         <Authenticated
             user={auth.user}
@@ -18,9 +19,52 @@ export default function Index({ auth, projects }) {
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="overflow-auto space-y-4">
-                                {data.map((project) => (
-                                    <div className='bg-gray-700/50 rounded px-3 py-1.5' key={project.id}>{project.name}</div>
-                                ))}
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <th className="px-3 py-2">ID</th>
+                                            <th className="px-3 py-2">Image</th>
+                                            <th className="px-3 py-2">Name</th>
+                                            <th className="px-3 py-2">
+                                                Status
+                                            </th>
+                                            <th className="px-3 py-2">
+                                                Created Date
+                                            </th>
+                                            <th className="px-3 py-2">
+                                                Created By
+                                            </th>
+                                            <th className="px-3 py-2"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.map((project) => (
+                                            <tr
+                                                key={project.id}
+                                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                            >
+                                                <td className="px-3 py-2">
+                                                    {project.id}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    <img src={project.image_path} className='h-20 w-auto'/>
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    {project.name}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    {project.status}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    {project.created_at}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    {project.createdBy.name}
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
